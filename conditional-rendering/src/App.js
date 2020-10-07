f809fa2962017fee3b555a21917c08eee3890939
nawan44 <rachmat.d.gunawan.com>
2020-10-07 23:27:05 +0700

    conditional rendering

---
+++ conditional-rendering/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: true,
+    };
+  }
+  logoutMethod = () => {
+    this.setState({ isLoggedIn: false })
+  }
+  loginMethod = () => {
+    this.setState({ isLoggedIn: true })
+  }
+
+  render() {
+    let { isLoggedIn } = this.state;
+    const renderButton = () => {
+      if (isLoggedIn) {
+        return <button onClick={this.logoutMethod}>Logout</button>
+      }
+      else {
+        return <button onClick={this.loginMethod}>Login</button>
+      }
+    }
+    return (
+      <div className="App">
+        <h1>Conditional Rendering</h1>
+        {renderButton()}
+      </div>
+    );
+  }
+}
+export default App;
