d13b4f339c31b0b7bd7b0202451be35d8da421e3
nawan44 <rachmat.d.gunawan.com>
2020-11-02 15:25:04 +0700

    reactstrap

---
+++ case-study-todolist/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div >
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ case-study-todolist/src/component/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="card-header">
+            <h1 className="title-header">You Have Task</h1>
+        </div>
+    )
+}
