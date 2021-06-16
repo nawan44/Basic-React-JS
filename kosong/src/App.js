12f7ba4e411cce4c3bb0b7b670d9cbbe4ddeefeb
rachmat.gunawan <rachmat.gunawan@delameta.com>
2021-06-16 11:28:51 +0700

    calculate age auto

--- kosong/src/App.js
+++ kosong/src/App.js
@@ -1,69 +1,46 @@
-import React, { useState } from "react";
+import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-const Row = function (props) {
-  const { checked, value, onChange, onChecked } = props;
+import { TextField } from "@material-ui/core";
+import moment from "moment";
+var ageCalculator = require("age-calculator");
+let { AgeFromDateString, AgeFromDate } = require("age-calculator");
+
+function App() {
+  const [dem_pat_birthyear, setCekUmur] = useState();
+
+  let ageFromDate = new AgeFromDate(new Date(1987, 0, 8)).age;
+  console.log("value from AgeFromDate", ageFromDate);
+
+  // var a = moment();
+  // var b = moment({dem_pat_birthyear}, "MM-YYYY");
+  // var age = moment.duration(a.diff(b));
+  // var years = age.years();
+  // var months = age.months();
+  // console.log("The age is " + years + " years " + months + " months ");
   return (
-    <div>
-      <input type="checkbox" checked={checked} onChange={onChecked} />
-      <input type="text" value={value} onChange={onChange} />
+    <div className="App">
+      {" "}
+      <TextField
+        id="outlined-basic"
+        label="Tanggal sekarang"
+        variant="outlined"
+        name="tanggalSekarang"
+        type="date"
+        InputLabelProps={{ shrink: true, required: true }}
+        // defaultValue={moment().format("YYYY-MM-DD")}
+        // defaultValue={moment("1995-12-25").format("YYYY-MM-DD")}
+        value={dem_pat_birthyear}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Umur"
+        variant="outlined"
+        InputLabelProps={{ shrink: true, required: true }}
+        value={ageFromDate}
+      />
     </div>
   );
-};
+}
 
-const App = function (props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     rows: [{ value: "", checked: false }],
-  //   };
-  // }
-  const [state, setState] = useState({
-    rows: [{ value: "", checked: false }],
-  });
-  const updateValue = (e, idx) => {
-    const rows = [...state.rows]; // copy array because we don't want to mutate the previous one
-    rows[idx].value = e.target.value;
-    setState({
-      rows,
-    });
-  };
-  const onChecked = (idx) => {
-    const rows = [...state.rows]; // copy array because we don't want to mutate the previous one
-    rows[idx].checked = !rows[idx].checked;
-    setState({
-      rows,
-    });
-  };
-  const addRow = () => {
-    const rows = [...state.rows, { value: "", checked: false }];
-    setState({
-      rows,
-    });
-  };
-  const deleteRows = () => {
-    const rows = [...state.rows, { value: "", checked: false }];
-
-    setState({
-      rows: state.rows.filter((e) => !e.checked),
-    });
-  };
-
-  return (
-    <div>
-      {state.rows.map((row, idx) => {
-        return (
-          <Row
-            key={idx}
-            value={row.value}
-            checked={row.checked}
-            onChange={(e) => updateValue(e, idx)}
-            onChecked={() => onChecked(idx)}
-          />
-        );
-      })}
-      <button onClick={addRow}>add</button>
-      <button onClick={deleteRows}>delete</button>
-    </div>
-  );
-};
 export default App;
