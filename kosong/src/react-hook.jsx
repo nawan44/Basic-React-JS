d8b9c4965a2e21679c36c7dfafea81206a1e454e
rachmat.gunawan <rachmat.gunawan@delameta.com>
2021-07-23 11:00:51 +0700

    cara pemakaian

--- kosong/src/react-hook.jsx
+++ kosong/src/react-hook.jsx
@@ -1,23 +1,18 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Autosuggest from "react-autosuggest";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, FormControl, MenuItem } from "@material-ui/core";
 
 const data = {
   alamat: "Jakarta, Bekasi",
   jumlah: [3, 4],
   nama: "rachmat, gunawan",
 };
-console.log("data", data);
-console.log("type of data", typeof data);
+
 const aa = [data];
 
 const as = data.nama.split(",");
 // console.log("ar", ar);
-console.log("as", as);
-
-// console.log("data type", Object.keys(data.nama));
-console.log("nama length", aa);
 
 const users = [
   {
@@ -84,7 +79,7 @@ function ReactHook() {
     );
   }
   const onNicknameSuggestionSelected = (event, { suggestion }) => {
-    console.log("ggggggg", suggestion);
+    // console.log("ggggggg", suggestion);
     setEmailValue(suggestion.email);
     setAlamatValue(suggestion.alamat);
   };
@@ -92,7 +87,7 @@ function ReactHook() {
     const newDoc = [...details_data];
     newDoc[index][e.target.name] = e.target.value;
     set_details_data(newDoc);
-    console.log("newDoc", newDoc);
+    // console.log("newDoc", newDoc);
   };
   // var result = data.map((person) => ({
   //   value: person.nama,
@@ -100,54 +95,45 @@ function ReactHook() {
   // }));
   // console.log(result);
 
+  // DROPDOWN
+  const filterRef = React.useRef();
+  const ref = useRef();
+  const [text, setText] = useState("");
+  const [menu, setMenu] = useState(null);
+
+  const [selectedOption, setSelectedOption] = React.useState("");
+  const [filterExpression, setFilterExpression] = React.useState("");
+  const [state, setState] = useState({
+    nama_obat: null,
+    id_obat: null,
+    frekuensi_pemakaian: null,
+    jumlah: null,
+    cara_pemakaian: null,
+  });
+  console.log("state", state);
+  const onChangeSelection = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleChangeCuk = (event) => {
+    const abc = { ...state, [event.target.name]: event.target.value };
+    setState(abc);
+  };
+  const stopImmediatePropagation = (e) => {
+    // filterRef.current.focus();
+    e.stopPropagation();
+    // e.preventDefault();
+  };
+  const onChangeExpression = (event) => {
+    const value = event.target.value.toString();
+    console.log(`value:`, value);
+    setState(value);
+  };
+
   return (
     <div className="container">
-      {/* <div>
-        <Autosuggest
-          suggestions={nicknameSuggestions}
-          onSuggestionsClearRequested={() => setNicknameSuggestions([])}
-          onSuggestionsFetchRequested={({ value }) => {
-            console.log("val", value);
-            setNicknameValue(value);
-            setNicknameSuggestions(getSuggestions(value));
-          }}
-          onSuggestionSelected={onNicknameSuggestionSelected}
-          getSuggestionValue={getSuggestionNickname}
-          renderSuggestion={renderSuggestion}
-          value={nicknameValue}
-          inputProps={{
-            placeholder: "Nama Karyawan ...",
-            value: nicknameValue,
-            onChange: (event, { newValue, method }) => {
-              console.log(newValue);
-              setNicknameValue(newValue);
-            },
-          }}
-          highlightFirstSuggestion={true}
-        />
-        <TextField
-          variant="outlined"
-          inputProps={{
-            placeholder: "Email ...",
-            value: emailValue,
-            onChange: (emailValue) => {
-              setEmailValue(emailValue);
-            },
-          }}
-        />
-        <TextField
-          variant="outlined"
-          inputProps={{
-            placeholder: "Email ...",
-            value: alamatValue,
-            onChange: (alamatValue) => {
-              setAlamatValue(alamatValue);
-            },
-          }}
-        />
-      </div>
-    */}
-
       <Grid spacing={1} container>
         {as &&
           as.map((key) => (
@@ -160,6 +146,88 @@ function ReactHook() {
             />
           ))}
       </Grid>
+      <FormControl style={{ marginTop: "20px 0 0 0" }}>
+        <TextField
+          select
+          margin="dense"
+          variant="outlined"
+          label="Cara Pakai"
+          style={{ width: "180px", margin: "30px 0 0 0 " }}
+          InputProps={{
+            className: "text6Props",
+          }}
+          // InputLabelProps={{ shrink: true }}
+          SelectProps={{
+            // onChange: onChangeSelection,
+            // value: state.cara_pemakaian,
+
+            MenuProps: {
+              className: "text6-menu-props",
+              getContentAnchorEl: null,
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left",
+              },
+              size: "small",
+              margin: "dense",
+            },
+          }}
+          id="cara_pemakaian"
+          name="cara_pemakaian"
+          onChange={handleChangeCuk}
+          value={state.cara_pemakaian}
+        >
+          <MenuItem value="Sebelum Makan" size="small" margin="dense">
+            Sebelum Makan
+          </MenuItem>
+          <MenuItem value="Setelah Makan" size="small" margin="dense">
+            Setelah Makan
+          </MenuItem>
+          <MenuItem value="Pemakaian Luar" size="small" margin="dense">
+            Pemakaian Luar
+          </MenuItem>
+          <MenuItem value="Ditetes" size="small" margin="dense">
+            Ditetes
+          </MenuItem>
+          <MenuItem
+            name="lain"
+            size="small"
+            margin="dense"
+            onKeyDown={(e) => e.stopPropagation()}
+            onClickCapture={stopImmediatePropagation}
+            // value={handleChangeCuk}
+            // onChange={onChangeSelection}
+            // TextFieldProps ={{
+            //   onKeyDown={(e) => e.stopPropagation()}
+            //   // onChange={(event, selectedOption) => selectedOption} // You can get the `selectedValue` inside your handler function on every time user select some new value
+            //   // inputRef={filterRef}
+            //   // onChange={() => {
+            //   //   handleChangeCuk;
+            //   //   console.log("change");
+            //   // }}
+            //   // name="cara_pemakaian"
+
+            // }}
+          >
+            <TextField
+              style={{ background: "red" }}
+              id="standard-full-width"
+              style={{ margin: 8 }}
+              label="Lain -lain"
+              placeholder="Lain -lain"
+              fullWidth
+              name="cara_pemakaian"
+              value={state.cara_pemakaian}
+              margin="dense"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={state.cara_pemakaian}
+              onChange={handleChangeCuk}
+            />
+          </MenuItem>
+        </TextField>
+      </FormControl>
     </div>
   );
 }
