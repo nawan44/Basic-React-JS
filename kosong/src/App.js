e2fdd44ded6168ddb395d3b0ced5af42d3c76129
rachmat.gunawan <rachmat.gunawan@delameta.com>
2021-08-26 18:21:04 +0700

    select textfield

--- kosong/src/App.js
+++ kosong/src/App.js
@@ -1,19 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import React from "react";
-import Autosuggest from "react-autosuggest";
 import ReactHook from "./react-hook";
-import ReactClass from "./react-class";
-import ReactContoh from "./reactContoh";
 
 function App() {
   return (
     <div className="container">
-      {/* <h3>Class Component</h3> */}
-      {/* <ReactClass /> */}
       <h3>Hook</h3>
       <ReactHook />
-      {/* <ReactContoh /> */}
     </div>
   );
 }
--- kosong/src/parent1.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-export default function Parent1() {
-  return <div>Parent 1</div>;
-}
